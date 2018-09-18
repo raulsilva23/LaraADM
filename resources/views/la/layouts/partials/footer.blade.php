47841bb5153922f7fda983512c4678cb50df9b40
Raul Silva <raulfgr2@gmail.com>
2018-09-18 14:20:08 -0300

    primeiro commit

---
+++ resources/views/la/layouts/partials/footer.blade.php
@@ -0,0 +1,8 @@
+@if(!isset($no_padding))
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        Powered by <a href="http://dwijitsolutions.com">FGR Arquitetura e Engenharia</a>
+    </div>
+    <strong>Copyright &copy; 2018
+</footer>
+@endif
